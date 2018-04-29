7a018579fba98b8e021e14b290f116a2231abfb0
Bemdell5 <Bemdell5@DESKTOP-R6DP7I7>
2018-04-29 19:42:06 +0300

    sayısal loto ekranlı yapıldı.

---
+++ UtilPrj/src/com/bilisimegitim/utilprj/sayisalloto/SayisalLoto.java
@@ -0,0 +1,62 @@
+package com.bilisimegitim.utilprj.sayisalloto;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+
+public class SayisalLoto {
+
+	public static final int MİNDEGER = 1;
+	public static final int MAXDEGER = 49;
+	public static final int ARRAY_BOY = 6;
+
+	public int[] arrayOlustur() {
+		
+		int[] sayilar = new int[ARRAY_BOY];
+         int sayi = 0;
+	//arrayin tüm elemanlarına sayı dodurulacak.
+		for (int i = 0; i < sayilar.length; i++) {
+		   
+		do {
+			
+		sayi = rastgeleSayiOlustur();
+		
+		} while (arraydeVarmi(sayi, sayilar));
+		
+		sayilar[0]= sayi;
+		
+		}
+		Arrays.sort(sayilar);
+		return sayilar;
+
+	}
+
+	private int rastgeleSayiOlustur()
+
+	{
+
+		Random rand = new Random();
+		return rand.nextInt(MAXDEGER) + 1;
+
+	}
+
+	private boolean arraydeVarmi(int p_sayi, int[] p_array) {
+		
+		//Arrays.binarysearch kullanmanın şartı arrayin sıralı olması
+		//olduğu için önce sıralıyorum.
+		Arrays.sort(p_array);
+		int sonuc =Arrays.binarySearch(p_array, p_sayi);
+
+	
+	if (sonuc >=0) {
+		 return true;
+	} else {
+
+	 return false;
+	}
+	
+	
+	
+	}
+}
